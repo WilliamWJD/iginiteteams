9616a1e11184eb0e80bbe9ffb1bb099a1ab87e50
WilliamWJD <william.jose.wjd@gmail.com>
2022-09-27 19:20:32 -0300

    feat: criando componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+
 import { Container } from "./styles";
+import { Alert } from "react-native";
 
 export function Groups() {
     return (
@@ -10,6 +13,10 @@ export function Groups() {
             <Highlight
                 title="Turmas"
                 subtitle="jogue com a sua turma"
+            />
+
+            <GroupCard
+                title="Galera do ignite"
             />
         </Container>
     )
