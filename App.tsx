f03f5296b8ab3dbcdd6acd4a32cb76c48454783d
WilliamWJD <william.jose.wjd@gmail.com>
2022-09-30 08:39:26 -0300

    feat: configurando o react-navigation

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-// import { Groups } from '@screens/Groups';
-import { NewGroup } from '@screens/NewGroup';
+import { Routes } from './src/routes';
 import { Loading } from "@components/Loading/index"
-import { Players } from '@screens/Player';
 
 import theme from '@theme/index';
 
@@ -22,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
