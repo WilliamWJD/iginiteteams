e64e1e495b3948ef45e7387decdd53ac2dcd1df3
WilliamWJD <william.jose.wjd@gmail.com>
2022-10-03 08:23:54 -0300

    feat: passando parametro group para a tela de players

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+    const [group, setGroup] = useState('');
+
     const navigation = useNavigation();
 
     function handleNew() {
-        navigation.navigate("players", { group: "Galerinha do FIFA" })
+        navigation.navigate("players", { group })
     }
 
     return (
@@ -21,7 +26,13 @@ export function NewGroup() {
                     title="Nova turma"
                     subtitle="Crie a turma para adicionar as pessoas"
                 />
-                <Input placeholder="Nome da turma" />
+
+                <Input
+                    placeholder="Nome da turma"
+                    onChangeText={setGroup}
+                    value={group}
+                />
+
                 <Button
                     title="Criar"
                     onPress={handleNew}
--- src/screens/Player/index.tsx
+++ src/screens/Player/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
@@ -12,16 +13,23 @@ import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+type RouteParams = {
+    group: string;
+}
+
 export function Players() {
     const [team, setTeam] = useState('Time a');
-    const [players, setPlayers] = useState(['William', 'Marcos', 'Vitor', 'Edvaldo', 'Teste', 'Elivelton', 'Karolaine', 'Narcisio', 'Robersval', 'Neymar', 'Ronaldo']);
+    const [players, setPlayers] = useState([]);
+
+    const route = useRoute();
+    const { group } = route.params as RouteParams;
 
     return (
         <Container>
             <Header showBackButton />
 
             <Highlight
-                title="Nome da turma"
+                title={group}
                 subtitle="adicione a galera e separe os times"
             />
 
