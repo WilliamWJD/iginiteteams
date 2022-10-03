370f89a01c03930911d9c8aae144820b6b964ccc
WilliamWJD <william.jose.wjd@gmail.com>
2022-10-03 08:15:53 -0300

    feat: aplicando o safeAreaView, e iniciando configuração de rotas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native';
+
 import imgLogo from '@assets/logo.png';
 
 import { BackIcon, Container, Logo, BackButton } from "./styles";
@@ -7,10 +9,16 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props) {
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.navigate("groups");
+    }
+
     return (
         <Container>
             {showBackButton && (
-                <BackButton>
+                <BackButton onPress={handleGoBack}>
                     <BackIcon />
                 </BackButton>
             )}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,9 +2,16 @@ import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+    const navigation = useNavigation();
+
+    function handleNew() {
+        navigation.navigate("players", { group: "Galerinha do FIFA" })
+    }
+
     return (
         <Container>
             <Header showBackButton />
@@ -15,7 +22,10 @@ export function NewGroup() {
                     subtitle="Crie a turma para adicionar as pessoas"
                 />
                 <Input placeholder="Nome da turma" />
-                <Button title="Criar" />
+                <Button
+                    title="Criar"
+                    onPress={handleNew}
+                />
             </Content>
         </Container>
     )
