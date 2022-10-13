0dd02d7fb865b5d5953cbaa7695592ebcec2d934
WilliamWJD <william.jose.wjd@gmail.com>
2022-10-13 08:26:58 -0300

    feat: melhorando a usabilidade do usuário

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,12 @@
-import { TextInputProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 import { Container } from "./styles";
 
-export function Input({ ...rest }: TextInputProps) {
+type Props = TextInputProps & {
+    inputRef?: React.RefObject<TextInput>;
+}
+
+export function Input({ inputRef, ...rest }: Props) {
     return (
-        <Container {...rest} />
+        <Container ref={inputRef} {...rest} />
     )
 }
--- src/screens/Player/index.tsx
+++ src/screens/Player/index.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { Alert, FlatList } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { Alert, FlatList, TextInput } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import { ButtonIcon } from '@components/ButtonIcon';
@@ -30,6 +30,8 @@ export function Players() {
     const route = useRoute();
     const { group } = route.params as RouteParams;
 
+    const newPlayerNameInputRef = useRef<TextInput>(null);
+
     async function handleAddPlayer() {
         if (newPlayerName.trim().length === 0) {
             return Alert.alert("Nova pessoa", "Informe o nome da pessoa para adicionar");
@@ -42,6 +44,10 @@ export function Players() {
 
         try {
             await playerAddByGroup(newPlayer, group);
+
+            newPlayerNameInputRef.current?.blur();
+
+            setNewPlayerName('');
             fetchPlayersByTeam();
         } catch (error) {
             if (error instanceof AppError) {
@@ -78,10 +84,13 @@ export function Players() {
 
             <Form>
                 <Input
+                    inputRef={newPlayerNameInputRef}
                     placeholder="Nome da pessoa"
                     autoCorrect={false}
                     onChangeText={setNewPlayerName}
                     value={newPlayerName}
+                    onSubmitEditing={handleAddPlayer}
+                    returnKeyType="done"
                 />
                 <ButtonIcon
                     icon="add"
